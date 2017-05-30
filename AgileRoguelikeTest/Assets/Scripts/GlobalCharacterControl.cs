ee37f88d79103fd67d2c06041e23646fda367cf1
jddrago <jason@delorie.com>
2017-05-30 13:06:52 -0600

    working on saving between scenes

---
+++ AgileRoguelikeTest/Assets/Scripts/GlobalCharacterControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCharacterControl : MonoBehaviour {
+
+    public static GlobalCharacterControl Instance;
+    public GameObject Char;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Global character destroyed.");
+    }
+}
---
+++ AgileRoguelikeTest/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public GameObject Char;
+
+	// Use this for initialization
+	void Start () {
+        Char = GlobalCharacterControl.Instance.Char;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SaveToGlobal()
+    {
+        GlobalCharacterControl.Instance.Char = Char;
+    }
+
+}
